704c97ff79f95fba56b6ff4d7f113ba04760dc52
Caolan <barryc10@tcd.ie>
2020-01-19 23:17:11 +0000

    got collisions of the pieces with the slots working

--- AR_Tops/Assets/Scripts/MyScripts/MyPiecePlacer.cs
+++ AR_Tops/Assets/Scripts/MyScripts/MyPiecePlacer.cs
@@ -11,7 +11,7 @@ public class MyPiecePlacer : MonoBehaviour
 	public GameObject[] slots;
 
 	// The bounds of the slots
-	public Bounds[] slotsBounds;	
+	public Collider[] slotsColliders;	
 	
 	// The parent game object
 	private GameObject slotsParent;
@@ -20,45 +20,45 @@ public class MyPiecePlacer : MonoBehaviour
 	private string parent_name = "Connect_4_Board_Slots";
 	
 	// This objects bounds
-	private Mesh this_mesh;
+	public Collider this_collider;
 
-	// This objects bounds
-	public Bounds this_bounds;
-
-	// Test Bounds
-	public Bounds first_bound;
-
-	// First slot
-	public GameObject first_slot;
-
+	// Used to determine whether to take control away from the user
+	public bool isColliding;
+	
+	// The Slot it is colliding with
+	public GameObject colliding_slot;
 
 	void Start()
 	{
 		slotsParent = GameObject.Find(parent_name);
 
-		this_mesh = GetComponent<MeshFilter>().mesh;
-
-		this_bounds = this_mesh.bounds;
+		this_collider = GetComponent<CapsuleCollider>();
 
 		int i = 0;
 		foreach(Transform t in slotsParent.transform)
 		{
-			slots[i++] = t.gameObject;
-			// slotsBounds[i++] = t.gameObject.transform.GetChild(0).gameObject.GetComponent<MeshFilter>().mesh.bounds;
+			slots[i] = t.gameObject;
+			slotsColliders[i] = t.gameObject.GetComponent<SphereCollider>();
+			i += 1;
 		}
-		first_slot = slotsParent.transform.GetChild(0).GetChild(0).gameObject;
-		first_bound = first_slot.GetComponent<MeshFilter>().mesh.bounds;
     }
 
     void Update()
     {
-		this_mesh = GetComponent<MeshFilter>().mesh;
+		this_collider = GetComponent<CapsuleCollider>();
 
-		this_bounds = this_mesh.bounds;
-
-		if (this_bounds.Intersects(first_bound))
+		foreach (Collider c in slotsColliders)
 		{
-			Debug.Log("Intersecting");
+			if (this_collider.bounds.Intersects(c.bounds))
+			{
+				isColliding = true;
+				colliding_slot = c.gameObject;
+			}
+			else
+			{
+				isColliding = false;
+				colliding_slot = null;
+			}
 		}
     }
 }
