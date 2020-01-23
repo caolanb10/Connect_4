cc49e60e42d0980dd277d4a809078b4cd1387f0d
Caolan <barryc10@tcd.ie>
2020-01-23 01:20:57 +0000

    updated prefab to have all spawn points for when the players are connected

---
+++ AR_Tops/Assets/Scripts/MyScripts/MyNetworkGameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class MyNetworkGameManager : MonoBehaviourPunCallbacks
+{
+	[Header("Room Setup")]
+	public byte MaxPlayers;
+
+	[Header("UI Inform")]
+	public GameObject UI_Inform_Panel;
+	public TextMeshProUGUI UI_Inform_Text;
+	public GameObject SearchForGamesButton;
+
+	[Header("String Constants")]
+	private string notSearching = "Search for Games to Play Connect 4";
+	private string searching = "Searching for room ....";
+
+	void Start()
+	{
+		UI_Inform_Panel.SetActive(true);
+		UI_Inform_Text.text = notSearching;
+	}
+
+	private void CreateAndJoinRoom()
+	{
+		// Create new options object and set max players to be equal to 2
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = MaxPlayers;
+
+		// Create room with random integer ID
+		string randomRoomName = "Room " + Random.Range(0, 1000);
+		PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+	}
+
+	#region UI Callbacks
+	public void JoinRandomRoom()
+	{
+		UI_Inform_Text.text = searching;
+		PhotonNetwork.JoinRandomRoom();
+		SearchForGamesButton.SetActive(false);
+	}
+	#endregion
+
+	#region Photon Callback Mehtods
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		UI_Inform_Text.text = message;
+		CreateAndJoinRoom();
+	}
+
+	public override void OnPlayerEnteredRoom(Player newPlayer)
+	{
+		Debug.Log(newPlayer.NickName + " has just joined to " + PhotonNetwork.CurrentRoom.Name + " Player Count is " + PhotonNetwork.CurrentRoom.PlayerCount);
+		if (PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayers)
+		{
+			UI_Inform_Text.text = "New Player called " + newPlayer.NickName + " has just joined the room. Game Starting";
+			StartCoroutine(DeactivateAfterSeconds(UI_Inform_Panel, 2.0f));
+		}
+	}
+	#endregion
+
+	IEnumerator DeactivateAfterSeconds(GameObject gameObj, float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		gameObj.SetActive(false);
+	}
+}
---
+++ AR_Tops/Assets/Scripts/MyScripts/MySpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MySpawnManager : MonoBehaviourPunCallbacks
+{
+
+	public GameObject Player1Camera;
+	public GameObject Player2Camera;
+
+	public int NumberOfPieces;
+
+	public bool IsRoomEmpty = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public override void OnJoinedRoom()
+	{
+		if (PhotonNetwork.IsConnectedAndReady)
+		{
+			if (IsRoomEmpty)
+			{
+
+				// Place yellow pieces into the game first
+
+				PhotonNetwork.Instantiate();
+			}
+		}
+	}
+}
