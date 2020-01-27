c74a4044589b3e061bec0892a4bc1535ab4da94d
Caolan <barryc10@tcd.ie>
2020-01-27 01:11:13 +0000

    first commit for building to android and using AR to place the connect 4 board

---
+++ AR_Tops/Assets/Scripts/MyScripts/MyARPlacementAndPlaneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class MyARPlacementAndPlaneController : MonoBehaviour
+{
+
+	ARPlaneManager ARPlaneManager;
+	MyARPlacementManager MyARPlacementManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		ARPlaneManager = GetComponent<ARPlaneManager>();
+		MyARPlacementManager = GetComponent<MyARPlacementManager>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void DisableARPlacementAndPlaneDetection()
+	{
+		ARPlaneManager.enabled = false;
+		MyARPlacementManager.enabled = false;
+
+		SetAllPlanesActiveOrDeactive(false);
+	}
+
+
+	public void EnableARPlacementAndPlaneDetection()
+	{
+		ARPlaneManager.enabled = true;
+		MyARPlacementManager.enabled = true;
+
+		SetAllPlanesActiveOrDeactive(true);
+	}
+	private void SetAllPlanesActiveOrDeactive(bool value)
+	{
+		foreach(var plane in ARPlaneManager.trackables)
+		{
+			plane.gameObject.SetActive(value);
+		}
+	}
+}
---
+++ AR_Tops/Assets/Scripts/MyScripts/MyARPlacementManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class MyARPlacementManager : MonoBehaviour
+{
+	ARRaycastManager RayManager;
+	static List<ARRaycastHit> RayHits = new List<ARRaycastHit>();
+	public Camera ARCamera;
+	public GameObject Connect4Board;
+
+	public void Awake()
+	{
+		RayManager = GetComponent<ARRaycastManager>();
+	}
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 centerOfScreen = new Vector3(Screen.width / 2, Screen.height / 2);
+		Ray ray = ARCamera.ScreenPointToRay(centerOfScreen); 
+		
+		if(RayManager.Raycast(ray, RayHits, TrackableType.PlaneWithinPolygon))
+		{
+			// First intersection
+			Pose hitPose = RayHits[0].pose;
+
+			Vector3 positionToBePlaced = hitPose.position;
+
+			Connect4Board.transform.position = positionToBePlaced;
+		}
+    }
+}
