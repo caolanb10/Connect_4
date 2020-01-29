badf4ff08ac498b2247a610dfb17bab4f91252dd
Caolan <barryc10@tcd.ie>
2020-01-29 18:41:35 +0000

    making UI logic script

---
+++ AR_Tops/Assets/Scripts/MyScripts/MyUIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyUIManager : MonoBehaviour
+{
+	public GameObject SearchForGamesButton;
+	public GameObject UI_InformPanel;
+	public GameObject Placement_Buttons;
+	public GameObject Change_Rotation_Buttons;
+	public GameObject Change_Scale_Buttons;
+	public GameObject 
+	public GameObject
+	public GameObject
+	public GameObject
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
