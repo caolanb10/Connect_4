767f18fba0498042d9d2d749cc58bda85e3c8673
Caolan <barryc10@tcd.ie>
2020-02-03 23:05:00 +0000

    fixed red material and refactored the spawning script

--- AR_Tops/Assets/Scripts/MyScripts/MySpawnManager.cs
+++ AR_Tops/Assets/Scripts/MyScripts/MySpawnManager.cs
@@ -8,8 +8,6 @@ public class MySpawnManager : MonoBehaviourPunCallbacks
 	// Parent of gameplay objects
 	public GameObject GameplayObjects;
 
-	public GameObject PieceController;
-
 	public GameObject BoardParent;
 
 	// Piece spawn positions for red and yellow
@@ -18,50 +16,34 @@ public class MySpawnManager : MonoBehaviourPunCallbacks
 
 	public int NumberOfPieces;
 
-	public bool IsRoomEmpty = true;
-
-	private string YellowPiecePrefabName = "Connect_4_Piece_Yellow";
-	private string RedPiecePrefabName = "Connect_4_Piece_Red";
-
 	public override void OnJoinedRoom()
 	{
 		if (PhotonNetwork.IsConnectedAndReady)
 		{
-			MyARPlacementManager placementManager = GameObject.Find("AR Session Origin").GetComponent<MyARPlacementManager>();
-
-			Debug.Log("Rotation of board is " + BoardParent.transform.rotation.ToString());
-
-			// Place yellow pieces into the game first
-			if ((int) PhotonNetwork.CurrentRoom.PlayerCount == 1)
+			for(int i = 0; i < NumberOfPieces; i++)
 			{
-				Debug.Log("Spawning Yellow  Pieces");
-				for(int i = 0; i < NumberOfPieces; i++)
-				{
-					Transform YellowSpawnPositionTransform = YellowSpawnPositionsParent.transform.GetChild(i).transform;
-					Vector3 spawnPosition = YellowSpawnPositionTransform.position;
-					GameObject piece = (GameObject) PhotonNetwork.Instantiate(YellowPiecePrefabName, spawnPosition, Quaternion.identity);
-					piece.transform.Rotate(Vector3.up, placementManager.totalAngle);
-					Debug.Log(piece.transform.rotation.ToString());
-				}
-
-				// Enable controller and gameplay manager
-				GameplayObjects.SetActive(true);
+				SpawnPlayer(i);
 			}
-			
-			// Red pieces are for player 2
-			else
-			{
-				Debug.Log("Spawning Red Pieces Pieces");
-				for (int i = 0; i < NumberOfPieces; i++)
-				{
-					Transform RedSpawnPositionTransform = RedSpawnPositionsParent.transform.GetChild(i).transform;
-					Vector3 spawnPosition = RedSpawnPositionTransform.position;
-					GameObject piece = PhotonNetwork.Instantiate(RedPiecePrefabName, spawnPosition, Quaternion.identity);
-				}
-
-				// Enable controller and gameplay manager
-				GameplayObjects.SetActive(true);
-			}
+			GameplayObjects.SetActive(true);
 		}
 	}
+
+	#region Private Methods
+	private void SpawnPlayer(int i)
+	{
+		// Spawn yellow pieces for first player, red pieces for second player
+		bool isFirstPlayer = (int)PhotonNetwork.CurrentRoom.PlayerCount == 1;
+
+		Debug.Log("Are the first player: " + isFirstPlayer);
+
+		Vector3 spawnPosition = isFirstPlayer
+			? YellowSpawnPositionsParent.transform.GetChild(i).transform.position
+			: RedSpawnPositionsParent.transform.GetChild(i).transform.position;
+
+		GameObject piece = isFirstPlayer
+			? (GameObject)PhotonNetwork.Instantiate("Connect_4_Piece_Yellow", spawnPosition, Quaternion.identity)
+			: (GameObject)PhotonNetwork.Instantiate("Connect_4_Piece_Red", spawnPosition, Quaternion.identity);
+	}
+
+	#endregion
 }
